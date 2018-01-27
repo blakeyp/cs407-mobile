d84fe6040ce67fb65ae729c48c74712d6dc5e5eb
lonk44 <b.t.campbell@warwick.ac.uk>
2018-01-27 18:03:26 +0000

    Added Navigation drawer

--- app/src/main/java/patchworks/activities/MainActivity.java
+++ app/src/main/java/patchworks/activities/MainActivity.java
@@ -9,34 +9,28 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.net.InetAddress;
-import java.util.ArrayList;
-
 import patchworks.fragments.GameFinderFragment;
-import patchworks.fragments.LevelEditorFragment;
-import patchworks.fragments.LevelRuntimeFragment;
-import patchworks.utils.Game;
-import patchworks.adapters.GameAdapter;
 import patchworks.R;
+import patchworks.fragments.LevelBrowserFragment;
 
 public class MainActivity extends AppCompatActivity
         implements GameFinderFragment.OnFragmentInteractionListener{
 
     private android.support.v7.app.ActionBar mActionBar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,9 +39,10 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_levels:
+                    load_fragment("levels");
                     return true;
                 case R.id.navigation_controller:
-                    load_fragment();
+                    load_fragment("games");
                     return true;
                 case R.id.navigation_settings:
                     return true;
@@ -60,7 +55,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level_browser);
+        setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -68,14 +63,33 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mActionBar = getSupportActionBar();
         mActionBar.setTitle("Patchworks");
-        mActionBar.setDisplayHomeAsUpEnabled(false);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+
 
     }
 
-
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -86,6 +100,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case R.id.action_connect:
                 //create alert dialog builder
@@ -138,16 +155,26 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
-    private void load_fragment() {
+    private void load_fragment(String fragmentTag) {
         // load initial LevelEditorFragment fragment (default controller)
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        Fragment fragment = new GameFinderFragment();
+        Fragment fragment;
 
-        // overlay fragment onto activity
-        fragmentTransaction.replace(R.id.fragment_space, fragment);
-        fragmentTransaction.commit();
+        switch (fragmentTag) {
+            case "levels":
+                fragment = new LevelBrowserFragment();
+                fragmentTransaction.replace(R.id.fragment_space, fragment);
+                fragmentTransaction.commit();
+                break;
+
+            case "games":
+                fragment = new GameFinderFragment();
+                fragmentTransaction.replace(R.id.fragment_space, fragment);
+                fragmentTransaction.commit();
+        }
+
     }
 
     @Override
---
+++ app/src/main/java/patchworks/fragments/LevelBrowserFragment.java
@@ -0,0 +1,30 @@
+package patchworks.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import patchworks.R;
+
+/**
+ * Created by u1421499 on 27/01/18.
+ */
+
+public class LevelBrowserFragment extends Fragment {
+
+    public LevelBrowserFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browser, container, false);
+    }
+}
